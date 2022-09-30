673472f8be44aca11f3565e6485dd0eb83622a89
pchchv <ipchchv@gmail.com>
2022-09-30 10:31:33 +0300

    implement a method Open

--- kvs.go
+++ kvs.go
@@ -1,7 +1,38 @@
 package kvs
 
-import "github.com/boltdb/bolt"
+import (
+	"time"
 
+	"github.com/boltdb/bolt"
+)
+
+// Key value store
+// Use the Open() method to create one, and Close() it when done
 type Store struct {
+	// TODO: Remove dependence on boltDB. Implement all necessary functionality
 	db *bolt.DB
+}
+
+// Open a key-value store
+// "path" is the full path to the database file, any leading directories must have been created already
+// File is created with mode 0640 if needed
+// Because of BoltDB restrictions, only one process may open the file at a time
+// Attempts to open the file from another process will fail with a timeout error
+func Open(path string) (*Store, error) {
+	opts := &bolt.Options{
+		Timeout: 75 * time.Millisecond,
+	}
+	if db, err := bolt.Open(path, 0640, opts); err != nil {
+		return nil, err
+	} else {
+		err := db.Update(func(tx *bolt.Tx) error {
+			_, err := tx.CreateBucketIfNotExists([]byte("kv"))
+			return err
+		})
+		if err != nil {
+			return nil, err
+		} else {
+			return &Store{db: db}, nil
+		}
+	}
 }
